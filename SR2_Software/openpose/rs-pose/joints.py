236b3e08078b55ef00e3bb426b5a84b7411825f3
Jerry Goss <jerry.h.goss@gmail.com>
2019-11-01 12:54:14 -0400

    Updating angles for keypoints of joints on openpose code; removed visualization for frame rate optimizaiton

--- SR2_Software/openpose/rs-pose/joints.py
+++ SR2_Software/openpose/rs-pose/joints.py
@@ -5,6 +5,22 @@ from sys import platform
 import argparse
 import pprint
 import time
+import numpy as np
+from itertools import chain
+
+def calc_angle(vals, points = None):
+    if points is None:
+        points = []
+    points.extend(v for v in vals)
+    # print(points)
+    ba = points[0] - points[1]
+    bc = points[2] - points[1]
+
+    cosine_angle = np.dot(ba, bc) / (np.linalg.norm(ba) * np.linalg.norm(bc))
+    angle = np.degrees(np.arccos(cosine_angle))
+
+    # pprint.pprint(np.degrees(angle))
+    return angle
 
 def main():
     try:
@@ -40,7 +56,7 @@ def main():
 
     num_frames = 0
     fps = '-'
-
+    angles = []
     while True:
         if not num_frames:
             start = time.time()
@@ -67,17 +83,31 @@ def main():
         if num_frames >= 10:
             end = time.time()
             fps = round(num_frames / (end - start), 2)
+                # rounds to two decimals, takes frames / time elapsed
             num_frames = 0
+        pprint.pprint(fps)
+
+
+        for item in range(len(keypoints)-1):
+                #iterates through vals in keypoints
+            thing = list(chain.from_iterable(keypoints[item]))
+            # print(thing)
+            if (len(thing) == 3):
+                angle = (calc_angle(thing))
+            angles.append(angle)
+                # un-nests list and returns angle
+            pprint.pprint(angles)
 
         # Render frame
-        im = datum.cvOutputData
-        cv2.putText(im, 'FPS: ' + str(fps), (10, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
-        cv2.imshow('Pose Estimation', im)
+        # im = datum.cvOutputData
+        # cv2.putText(im, 'FPS: ' + str(fps), (10, 30), font, 1, (0, 0, 0), 2, cv2.LINE_AA)
+        # cv2.imshow('Pose Estimation', im)
+
+
 
         key = cv2.waitKey(1)
         if key == ord('q'):
-            break 
-
+            break
 
     stream.release()
     cv2.destroyAllWindows()
